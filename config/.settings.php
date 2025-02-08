10b19e1c5f886feb6a333175bef0c984521cc5a4
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 15:28:00 +0300

    Файл конфигурации настроек виджета

---
+++ config/.settings.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации настроек виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'odnoklassniki' => false,
+    'surfingbird'   => false,
+    'messenger' => false,
+    'vkontakte' => false,
+    'blogger'   => false,
+    'delicious' => false,
+    'digg'      => false,
+    'evernote'  => false,
+    'linkedin'  => false,
+    'lj'        => false,
+    'moimir'    => false,
+    'pinterest' => false,
+    'pocket'    => false,
+    'qzone'     => false,
+    'reddit'    => false,
+    'renren'    => false,
+    'sinaWeibo' => false,
+    'skype'     => false,
+    'telegram'  => false,
+    'tumblr'    => false,
+    'twitter'   => false,
+    'viber'     => false,
+    'whatsapp'  => false,
+    'color'     => '', // цветовая схема кнопок
+    'size'      => '', // размер кнопок
+    'shape'     => '', // форма кнопок
+    'limit'     => '', // лимит кнопок
+    'position'  => 'before', // положение в статье 
+    'align'     => 'left', // выравнивание кнопок в списке
+    'popupDirection' => '', // направление открытия меню
+    'popupPosition'  => '', // расположение меню
+    'direction'      => '' // направление списка кнопок
+];
