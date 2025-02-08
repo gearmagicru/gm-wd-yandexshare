425c16cdcd2ef491e99d757448df8abf92d9c296
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 15:35:01 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Yandex Share',
+    '{description}' => 'Yandex Share',
+
+   // Settings
+   '{settings.title}' => 'Widget settings "Yandex Share"',
+   // Settings: поля
+   'Button color scheme' => 'Button color scheme',
+   'Social media button color scheme' => 'Social media button color scheme',
+   'Button size' => 'Button size',
+   'Social media button size' => 'Social media button size',
+   'Button shape' => 'Button shape',
+   'Form of social media buttons' => 'Form of social media buttons',
+   'Button limit' => 'Button limit',
+   'The number of social networks displayed as buttons' 
+       => 'The number of social networks displayed as buttons. It is used if you need to embed a lot of social networks into a block, and also so that the block takes up little space on the page. Social networks that are not included in the limit will be displayed in the menu when you press the button.',
+   'Square' => 'Square',
+   'Round' => 'Round',
+   'Classic' => 'Classic',
+   'Monochrome' => 'Monochrome',
+   'Monochrome 2' => 'Monochrome 2',
+   'Position in the article' => 'Position in the article',
+   'Before the article text' => 'Before the article text',
+   'After the article text' => 'After the article text',
+   'List alignment' => 'Aligning buttons in a list',
+   'Left' => 'Left',
+   'Right' => 'Right',
+   'Menu opening direction' => 'Menu opening direction',
+   'Auto' => 'Auto',
+   'Bottom' => 'Bottom',
+   'Top' => 'Top',
+   'Menu opening direction: auto - down by default; if it doesn’t fit down, then up; if it doesn’t fit either down or up, then down' 
+       => 'Menu opening direction: auto - down by default; if it doesn’t fit down, then up; if it doesn’t fit either down or up, then down',
+   'Button list direction' => 'Button list direction',
+   'Horizontal' => 'Horizontal',
+   'Vertical' => 'Vertical',
+   'Menu location' => 'Menu location',
+   'The location of the menu relative to the block container. The outer value may be needed if, due to the specific layout of your site, the pop-up is cut off by adjacent page elements.' 
+       => 'The location of the menu relative to the block container. The outer value may be needed if, due to the specific layout of your site, the pop-up is cut off by adjacent page elements.',
+   'Inner' => 'Inside the container',
+   'Outer' => 'Outside the container',
+  // Settings: всплывающие сообщения / текст
+   'Widget settings successfully changed' => 'Widget settings "Yandex Share" successfully changed.',
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Блок Яндекс «Поделиться»',
+    '{description}' => 'Публикация в своих социальных сетях ссылки на понравившиеся страницы сайта',
+
+    // Settings
+    '{settings.title}' => 'Настройка виджета Яндекс «Поделиться»',
+    // Settings: поля
+    'Yandex.Messenger' => 'Яндекс.Мессенджер',
+    'VKontakte' => 'ВКонтакте',
+    'Odnoklassniki' => 'Одноклассники',
+    'Moimir' => 'Мой Мир',
+    'Button color scheme' => 'Цветовая схема кнопок',
+    'Social media button color scheme' => 'Цветовая схема кнопок соцсетей',
+    'Button size' => 'Размер кнопок',
+    'Social media button size' => 'Размер кнопок соцсетей',
+    'Button shape' => 'Форма кнопок',
+    'Form of social media buttons' => 'Форма кнопок соцсетей',
+    'Button limit' => 'Лимит кнопок',
+    'The number of social networks displayed as buttons' 
+        => 'Количество соцсетей, отображаемых в виде кнопок. Используется если нужно встроить в блок много соцсетей, а также чтобы блок занимал мало места на странице. Не вошедшие в лимит соцсети будут отображаться в меню по нажатии кнопки.',
+    'Square' => 'Квадрат',
+    'Round' => 'Круг',
+    'Classic' => 'Классика',
+    'Monochrome' => 'Монохром',
+    'Monochrome 2' => 'Монохром 2',
+    'Position in the article' => 'Положение в статье',
+    'Before the article text' => 'До текста статьи',
+    'After the article text' => 'После текста статьи',
+    'List alignment' => 'Выравнивание кнопок в списке',
+    'Left' => 'Слева',
+    'Right' => 'Справа',
+    'Menu opening direction' => 'Направление открытия меню',
+    'Auto' => 'Авто',
+    'Bottom' => 'Вниз',
+    'Top' => 'Вверх',
+    'Menu opening direction: auto - down by default; if it doesn’t fit down, then up; if it doesn’t fit either down or up, then down' 
+        => 'Направление открытия меню: auto - по умолчанию вниз; если не вмещается вниз, то вверх; если не вмещается ни вниз, ни вверх, то вниз',
+    'Button list direction' => 'Направление списка кнопок',
+    'Horizontal' => 'Горизонтальное',
+    'Vertical' => 'Вертикальное',
+    'Menu location' => 'Расположение меню',
+    'The location of the menu relative to the block container. The outer value may be needed if, due to the specific layout of your site, the pop-up is cut off by adjacent page elements.' 
+        => 'Расположение меню относительно контейнера блока. Значение "Снаружи" может понадобиться в том случае, если из-за специфики верстки вашего сайта меню обрезается соседними элементами страницы.',
+    'Inner' => 'Внутри контейнера',
+    'Outer' => 'Снаружи контейнера',
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Настройки виджета "Блок Яндекс "Поделиться" успешно изменены.',
+];
